1c6140d71aa365c988af1b21e1f72737db8a5102
Rohit0445 <meenarohit458@gmail.com>
2025-05-02 12:49:53 +0530

    may2

---
+++ may1/script.js
@@ -0,0 +1,25 @@
+var age =25;
+var name = "rohit"
+console.log(name);
+console.log(age)
+
+//type of datatype
+
+var x
+console.log(x , typeof x)
+
+var firstName = "rohit"
+console.log(firstName, typeof firstName)
+
+var value = null
+console.log(value, typeof value)
+//datatype of null is an object (it is an mistake of java script)
+
+var something = false
+console.log(something, typeof something)
+
+var number = 84849555n
+console.log(number, typeof number)
+
+var id = symbol(id)
+console.log(id , typeof id)

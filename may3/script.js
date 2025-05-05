adc98547db5bf3e3f70959a2743fa1c4f2f3ee14
Rohit0445 <meenarohit458@gmail.com>
2025-05-05 11:47:21 +0530

    may3

---
+++ may3/script.js
@@ -0,0 +1,29 @@
+//strings 
+
+// let str = "Rohit Meena"
+
+// //methods
+
+// let x = str.length;
+// let y = str.charAt(4);
+// let z= str.indexOf("M");
+// let a = str.toUpperCase()
+// let b = str.toLocaleLowerCase()
+// let d = str.replaceAll("Rohit Meena","Suryansh Kapoor")
+// let e = str.slice(1,7)
+
+// console.log(x,y,z,a,b,d,e) 
+
+
+ let mystring = "developer "
+
+ let x= mystring.charAt(0)
+ let y= x.toUpperCase()
+
+ 
+
+ let z= mystring.slice(1 )
+
+ let a = y+z
+
+ console.log(a)
